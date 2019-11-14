d78269abb0794289b86e76bddfa6ff0f42f67c43
Nick Wang <nickyiliwang@gmail.com>
2019-11-14 18:17:19 -0500

    new transition

--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,53 @@
-// credit: https://codepen.io/RAN3000/pen/ogxwQK for basic logic
-const pawApp = {
-  endOfAnimations:
-    "webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", // detecting transition end
-  timesClicked: 0, //for special function events
-  // picture of paws
-  pawsSrc: ["./"],
-  // cubic-bezier values
-  cubicBezierArr: [
-    "all 3s cubic-bezier(.36,1.48,1,.71)", // Slow windup and bop
-    "all 2s cubic-bezier(.17,.67,.78,.36)",
-    "all 1s cubic-bezier(0,.98,.7,.7)",
-    "all 0.8s linear"
-  ],
-  // transform
-  transformArr: [
-    "translate(0, 50vw)",
-    "translate(0, 50vw)",
-    "translate(0, 50vw)"
-  ],
-  // positions
-  positionsArr: [
-    "right:5vw"
-  ],
-  randomNumber: max => {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
+const pawApp = {};
+// detecting transition end
+pawApp.endOfAnimations =
+  "webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend";
+// Tracking times user clicked
+pawApp.timesClicked = 0;
+// picture source of paws pictures
+pawApp.pawsSrc = ["./image/paws/Olives.png"];
+// cubic-bezier values for transitions variations
+pawApp.cubicBezierArr = [
+  "all 2s cubic-bezier(.36,1.48,1,.71)", // Slow windup and bop
+  "all 0.8s linear",
+  "all 2s cubic-bezier(.17,.67,.78,.36)",
+  "all 1s cubic-bezier(0,.98,.7,.7)"
+];
+// transform
+pawApp.transformArr = ["translate(0, 50vw)", "translate(0, 50vw)"];
+// positions
+pawApp.positionsArr = ["-70vw"];
+
+// functions
+pawApp.randomNumber = max => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+pawApp.animatePaws = () => {
+  $(".active").css("position", pawApp.positionsArr[0]);
+  $(".paws").css("transition", pawApp.cubicBezierArr[0]);
+};
+
+pawApp.activatePaws = () => {
+  $(".paws")
+    .addClass("active")
+    .one(pawApp.endOfAnimations, function() {
+      $("#lights").prop("checked", false);
+      $(".paws").removeClass("active");
+    });
 };
 
 // Functions
 pawApp.normalEvent = function() {
-  $(".switch").on("click", function() {
-    // $(".paws").css("transition", pawApp.cubicBezier[pawApp.randomNumber(3)]);
+  $(".switch").on("click", () => {
+    pawApp.animatePaws();
+    pawApp.activatePaws();
 
-    $(".paws").css(
-      "transition",
-      pawApp.cubicBezierArr[pawApp.timesClicked],
-      "transform",
-      pawApp.transformArr[pawApp.timesClicked],
-      "position",
-      pawApp.positionsArr[pawApp.timesClicked]
-    );
-    // pawApp.timesClicked++;
-
-    // $(".paws").css("transform", pawApp.transformArr[0]);
-    $(".paws")
-      .addClass("active")
-      .one(pawApp.endOfAnimations, function() {
-        $("#lights").prop("checked", false);
-        $(".paws")
-          .removeClass("active")
-          .one(pawApp.endOfAnimations);
-      });
+    $(".paws").addClass("active");
   });
 };
 
-pawApp.init = function() {
+pawApp.init = () => {
   pawApp.normalEvent();
 };
 
---
+++ scripts/snippets.js
@@ -0,0 +1,17 @@
+// using the randomizeFunction
+// $(".paws").css("transition", pawApp.cubicBezier[pawApp.randomNumber(3)]);
+
+// $(".pawsImage").attr("src", pawApp.pawsSrc[0]); // changes paws images
+
+// .css using clicking tracker
+// pawApp.cubicBezierArr[pawApp.timesClicked],
+// pawApp.transformArr[pawApp.timesClicked],
+// pawApp.positionsArr[pawApp.timesClicked]
+
+// "transform",
+// pawApp.transformArr[1],
+
+// "rotate(90deg) translate(-35vw, 120vw)",
+
+
+  // pawApp.timesClicked++;
