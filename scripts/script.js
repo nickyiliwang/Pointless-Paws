d994b0217e66dac78c25c75f6161c9a030f5f8b6
Nick Wang <nickyiliwang@gmail.com>
2019-11-16 21:54:55 -0500

    optimized switch statement

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,36 @@
+// DOM
+const lightSwitch = $(".lightSwitch");
+const paws = $(".paws");
+const body = $("body");
+
+// global object
 const pawApp = {};
 
 pawApp.init = () => {
   pawApp.endOfAnimations =
     "webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend";
   pawApp.timesClicked = 0;
-  pawApp.cubicBezierArr = [
-    "all 0.5s linear" // quick 3
+  pawApp.cubicBezier = [
+    "all 0.3s linear" // quick 3
   ];
+  pawApp.transformTranslate = ["translate(0, 50vw)"];
 };
 
 pawApp.activatePaws = () => {
   console.log("activatePaws");
-  $(".paws")
-    .addClass("active")
-    .one(pawApp.endOfAnimations, () => {
-      console.log("activatePaws animation end");
 
-      $(".paws").removeClass("active");
-      $("body").removeClass("lightsOut");
+  paws.addClass("active").one(pawApp.endOfAnimations, () => {
+    console.log("activatePaws animation end");
 
-      pawApp.deactivatePaws();
-    });
+    paws.removeClass("active");
+    body.removeClass("lightsOut");
+
+    pawApp.deactivatePaws();
+  });
 };
 
 pawApp.deactivatePaws = () => {
-  $(".paws").one(pawApp.endOfAnimations, () => {
+  paws.one(pawApp.endOfAnimations, () => {
     console.log("deactivatePaws animation end");
     pawApp.changePawsCss();
   });
@@ -32,55 +38,75 @@ pawApp.deactivatePaws = () => {
 
 pawApp.changePawsCss = () => {
   console.log("changePawsCss");
-  $(".paws").css("transition", pawApp.cubicBezierArr[0]);
-};
-
-// Functions
-pawApp.eventSwapper = () => {
-  console.log("swap event");
-  console.log(pawApp.timesClicked);
-
-  switch (pawApp.timesClicked) {
-    case 3:
-      console.log("quickThreeEvent selected 1");
-      pawApp.quickThreeEvent();
-      break;
-
-    default:
-      pawApp.normalEvent();
-      break;
-  }
+  paws.css("transition", pawApp.cubicBezier[0]);
 };
 
 pawApp.normalEvent = () => {
-  console.log("normalEvent run");
   pawApp.activatePaws();
-  $("body").addClass("lightsOut");
+  body.addClass("lightsOut");
+};
+
+pawApp.buttonMoveEvent = () => {
+  console.log("buttonMoveEvent active");
+  pawApp.normalEvent();
+  setTimeout(() => {
+    lightSwitch.css("transform", pawApp.transformTranslate);
+    lightSwitch.css("transition", pawApp.cubicBezier);
+  }, 500);
+
+  setTimeout(() => {
+    lightSwitch.css("transform", "none");
+    lightSwitch.css("transition", "none");
+  }, 2000);
 };
 
 pawApp.quickThreeEvent = () => {
-  console.log("quickThreeEvent run 2");
+  console.log("quickThreeEvent active");
   for (let i = 0; i < 3000; i += 1000) {
     setTimeout(() => {
-      $(".paws")
-        .css("transition", pawApp.cubicBezierArr[0])
+      body.addClass("lightsOut");
+      paws
+        .css("transition", pawApp.cubicBezier[0])
         .addClass("active")
         .one(pawApp.endOfAnimations, () => {
-          $(".paws").removeClass("active");
-          $("body").removeClass("lightsOut");
+          paws.removeClass("active");
+          body.removeClass("lightsOut");
         });
     }, i);
+  }
+  console.log("quick3done");
+};
+
+pawApp.addEventListenerToBtn = eventName => {
+  lightSwitch.one("click", () => {
+    pawApp.timesClicked++;
+    eventName();
+    pawApp.eventSwapper();
+  });
+};
+
+pawApp.eventSwapper = () => {
+  console.log("swap event");
+  console.log(pawApp.timesClicked);
+  switch (pawApp.timesClicked) {
+    case 1:
+      console.log("buttonMoveEvent selected");
+      pawApp.addEventListenerToBtn(pawApp.buttonMoveEvent);
+      break;
+
+    case 3:
+      console.log("quickThreeEvent run");
+      pawApp.quickThreeEvent();
+      pawApp.addEventListenerToBtn(pawApp.normalEvent);
+      break;
+
+    default:
+      pawApp.addEventListenerToBtn(pawApp.normalEvent);
+      break;
   }
 };
 
 $(function() {
   pawApp.init();
-
-  $(".lightSwitch").on("click", () => {
-    pawApp.timesClicked++;
-    pawApp.eventSwapper();
-  });
-
-  // pawApp.normalEvent();
-  // pawApp.quickThreeEvent();
+  pawApp.eventSwapper();
 });
